409831defdebfdd29ce8472b6411291ff2eb3a57
hydrocul <hydrocul@gmail.com>
2014-02-27 00:23:42 +0900

    testdataの中にスクリプトを置くだけで自動でテストスクリプトとして動かせる構成に変更

--- dotest.sh
+++ dotest.sh
@@ -11,8 +11,12 @@ mkdir -p actual
 
 RESULT=0
 
-sh ./testdata/v1/test.sh || RESULT=1
-sh ./testdata/v2/test.sh || RESULT=1
+(
+    echo "RESULT=0"
+    echo
+    ls ./testdata | perl -ne 'if(/^([^.]+)\.(v[0-9]+)\.rd$/){ if($2 eq "v2"){print "export ROONDA_EXPERIMENTAL=1\n"} else {print "export ROONDA_EXPERIMENTAL=\n"} print "echo ./testdata/$1.$2.rd\ncat ./testdata/$1.$2.rd | ./target/roonda --dry-run > ./actual/$1.$2.sh || RESULT=1\ndiff -u expected/$1.$2.sh actual/$1.$2.sh\ncat ./testdata/$1.$2.rd | ./target/roonda > ./actual/$1.$2.txt || RESULT=1\ndiff -u expected/$1.$2.txt actual/$1.$2.txt\n\n";}'
+    echo "exit \$RESULT"
+) | sh || RESULT=1
 
 if [ $RESULT != 0 ]; then
     echo "Failed!"
